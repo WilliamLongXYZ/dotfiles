82bc9c35173eaf0ddc99533c3912a2f9310031cc
William Long <will@williamlong.xyz>
2021-11-14 21:46:43 -0600

    Added my quick-staging script for easier access

---
+++ git-stage.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git add .xinitrc \
+        .config/yay \
+        .config/Litecoin \
+        .config/Bitcoin \
+        .config/htop \
+        .config/monero-project \
+        .config/newsboat \
+        .config/neofetch \
+        .config/sc-im \
+        .config/zecwallet \
+        .bashrc \
+        .surf/styles/default.css \
+        .vimrc \
+        .bash_profile \
+        .zshrc \
+        .config/shell \
+        git-stage.sh
